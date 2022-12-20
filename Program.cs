2af2b6303f018c3001dccc16a67701f146a459c4
Vacok1337 <121095327+Vacok1337@users.noreply.github.com>
2022-12-20 13:12:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Вы внесли = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Под процентную ставку = ");
+            double b = double.Parse(Console.ReadLine());
+            double c = a + (b * (a / 100));
+            Console.WriteLine("Через год вы получите " + c + "руб.");
+            Console.Read();
+        }
+    }
+}
